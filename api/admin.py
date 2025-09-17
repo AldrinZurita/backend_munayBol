3533bf84537e46faf3e2dfbeb3b111bdc31d572f
AldrinZurita <axzt2004@gmail.com>
2025-09-17 17:32:23 -0400

    merge

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Usuario, Hotel, LugarTuristico, Pago, Habitacion, Reserva, Paquete, Sugerencias
+
+admin.site.register(Usuario)
+admin.site.register(Hotel)
+admin.site.register(LugarTuristico)
+admin.site.register(Pago)
+admin.site.register(Habitacion)
+admin.site.register(Reserva)
+admin.site.register(Paquete)
+admin.site.register(Sugerencias)
